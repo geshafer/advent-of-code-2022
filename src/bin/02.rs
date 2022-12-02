d59d8d26ad3246897599c7123b4adb041e469e4b
Gale Shafer <gale.shafer@shopify.com>
2022-12-02 10:01:06 -0500

    Solve Day 2
    I'm not sure if it's because I spent some morning time on it and I'm
    feeling slightly better or if the problem was easier but, I had a much
    easier go on this problem. I thought about creating a struct to
    represent the hand signs but there are so few permutations that pattern
    matching seemed more pragmatic and I think I'm right. Luckily, they
    didn't bust out Spock or Lizard on part two.

---
+++ src/bin/02.rs
@@ -0,0 +1,64 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    input.split("\n").map(|round| {
+        if round.is_empty() {
+            Some(0)
+        } else {
+            match round.split_at(1) {
+                ("A", " X") => Some(1 + 3),
+                ("A", " Y") => Some(2 + 6),
+                ("A", " Z") => Some(3 + 0),
+                ("B", " X") => Some(1 + 0),
+                ("B", " Y") => Some(2 + 3),
+                ("B", " Z") => Some(3 + 6),
+                ("C", " X") => Some(1 + 6),
+                ("C", " Y") => Some(2 + 0),
+                ("C", " Z") => Some(3 + 3),
+                _ => Some(0),
+            }
+        }
+    }).sum()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    input.split("\n").map(|round| {
+        if round.is_empty() {
+            Some(0)
+        } else {
+            match round.split_at(1) {
+                ("A", " X") => Some(3 + 0),
+                ("A", " Y") => Some(1 + 3),
+                ("A", " Z") => Some(2 + 6),
+                ("B", " X") => Some(1 + 0),
+                ("B", " Y") => Some(2 + 3),
+                ("B", " Z") => Some(3 + 6),
+                ("C", " X") => Some(2 + 0),
+                ("C", " Y") => Some(3 + 3),
+                ("C", " Z") => Some(1 + 6),
+                _ => Some(0),
+            }
+        }
+    }).sum()
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 2);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 2);
+        assert_eq!(part_one(&input), Some(15));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 2);
+        assert_eq!(part_two(&input), Some(12));
+    }
+}
