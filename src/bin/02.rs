b111a1d9d90b8be8163b9053679ecc2c0c53a89e
Gale Shafer <gale.shafer@shopify.com>
2022-12-03 13:57:11 -0500

    Run cargo fmt

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,43 +1,49 @@
 pub fn part_one(input: &str) -> Option<u32> {
-    input.split("\n").map(|round| {
-        if round.is_empty() {
-            Some(0)
-        } else {
-            match round.split_at(1) {
-                ("A", " X") => Some(1 + 3),
-                ("A", " Y") => Some(2 + 6),
-                ("A", " Z") => Some(3 + 0),
-                ("B", " X") => Some(1 + 0),
-                ("B", " Y") => Some(2 + 3),
-                ("B", " Z") => Some(3 + 6),
-                ("C", " X") => Some(1 + 6),
-                ("C", " Y") => Some(2 + 0),
-                ("C", " Z") => Some(3 + 3),
-                _ => Some(0),
+    input
+        .split("\n")
+        .map(|round| {
+            if round.is_empty() {
+                Some(0)
+            } else {
+                match round.split_at(1) {
+                    ("A", " X") => Some(1 + 3),
+                    ("A", " Y") => Some(2 + 6),
+                    ("A", " Z") => Some(3 + 0),
+                    ("B", " X") => Some(1 + 0),
+                    ("B", " Y") => Some(2 + 3),
+                    ("B", " Z") => Some(3 + 6),
+                    ("C", " X") => Some(1 + 6),
+                    ("C", " Y") => Some(2 + 0),
+                    ("C", " Z") => Some(3 + 3),
+                    _ => Some(0),
+                }
             }
-        }
-    }).sum()
+        })
+        .sum()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    input.split("\n").map(|round| {
-        if round.is_empty() {
-            Some(0)
-        } else {
-            match round.split_at(1) {
-                ("A", " X") => Some(3 + 0),
-                ("A", " Y") => Some(1 + 3),
-                ("A", " Z") => Some(2 + 6),
-                ("B", " X") => Some(1 + 0),
-                ("B", " Y") => Some(2 + 3),
-                ("B", " Z") => Some(3 + 6),
-                ("C", " X") => Some(2 + 0),
-                ("C", " Y") => Some(3 + 3),
-                ("C", " Z") => Some(1 + 6),
-                _ => Some(0),
+    input
+        .split("\n")
+        .map(|round| {
+            if round.is_empty() {
+                Some(0)
+            } else {
+                match round.split_at(1) {
+                    ("A", " X") => Some(3 + 0),
+                    ("A", " Y") => Some(1 + 3),
+                    ("A", " Z") => Some(2 + 6),
+                    ("B", " X") => Some(1 + 0),
+                    ("B", " Y") => Some(2 + 3),
+                    ("B", " Z") => Some(3 + 6),
+                    ("C", " X") => Some(2 + 0),
+                    ("C", " Y") => Some(3 + 3),
+                    ("C", " Z") => Some(1 + 6),
+                    _ => Some(0),
+                }
             }
-        }
-    }).sum()
+        })
+        .sum()
 }
 
 fn main() {
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,48 +1,72 @@
 use std::collections::HashSet;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.split("\n").map(|rucksack| {
-        if rucksack.is_empty() {
-            0
-        } else {
-            let (one, two) = rucksack.split_at(rucksack.chars().count() / 2);
-            let compartment_one: HashSet<char> = HashSet::from_iter(one.chars().collect::<Vec<char>>());
-            let compartment_two: HashSet<char> = HashSet::from_iter(two.chars().collect::<Vec<char>>());
-
-            compartment_one.intersection(&compartment_two).map(|item| {
-                let priority = *item as u32 - 38;
-
-                if priority > 52 {
-                    priority - 58
+    Some(
+        input
+            .split("\n")
+            .map(|rucksack| {
+                if rucksack.is_empty() {
+                    0
                 } else {
-                    priority
+                    let (one, two) = rucksack.split_at(rucksack.chars().count() / 2);
+                    let compartment_one: HashSet<char> =
+                        HashSet::from_iter(one.chars().collect::<Vec<char>>());
+                    let compartment_two: HashSet<char> =
+                        HashSet::from_iter(two.chars().collect::<Vec<char>>());
+
+                    compartment_one
+                        .intersection(&compartment_two)
+                        .map(|item| {
+                            let priority = *item as u32 - 38;
+
+                            if priority > 52 {
+                                priority - 58
+                            } else {
+                                priority
+                            }
+                        })
+                        .last()
+                        .unwrap()
                 }
-            }).last().unwrap()
-        }
-    }).sum::<u32>())
+            })
+            .sum::<u32>(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(input.split("\n").filter_map(|rucksack: &str| -> Option<HashSet<char>> {
-        if rucksack.is_empty() {
-            None
-        } else {
-            Some(HashSet::from_iter(rucksack.chars().collect::<Vec<char>>()))
-        }
-    }).collect::<Vec<HashSet<char>>>().chunks(3).map(|group| -> u32 {
-        let one: HashSet<&char> = HashSet::from_iter(group[0].intersection(&group[1]).collect::<Vec<&char>>());
-        let two: HashSet<&char> = HashSet::from_iter(group[2].intersection(&group[1]).collect::<Vec<&char>>());
+    Some(
+        input
+            .split("\n")
+            .filter_map(|rucksack: &str| -> Option<HashSet<char>> {
+                if rucksack.is_empty() {
+                    None
+                } else {
+                    Some(HashSet::from_iter(rucksack.chars().collect::<Vec<char>>()))
+                }
+            })
+            .collect::<Vec<HashSet<char>>>()
+            .chunks(3)
+            .map(|group| -> u32 {
+                let one: HashSet<&char> =
+                    HashSet::from_iter(group[0].intersection(&group[1]).collect::<Vec<&char>>());
+                let two: HashSet<&char> =
+                    HashSet::from_iter(group[2].intersection(&group[1]).collect::<Vec<&char>>());
 
-        one.intersection(&two).map(|item| {
-            let priority = **item as u32 - 38;
+                one.intersection(&two)
+                    .map(|item| {
+                        let priority = **item as u32 - 38;
 
-            if priority > 52 {
-                priority - 58
-            } else {
-                priority
-            }
-        }).last().unwrap()
-    }).sum())
+                        if priority > 52 {
+                            priority - 58
+                        } else {
+                            priority
+                        }
+                    })
+                    .last()
+                    .unwrap()
+            })
+            .sum(),
+    )
 }
 
 fn main() {
